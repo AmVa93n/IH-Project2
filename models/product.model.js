0456b9187edc0a46a3632361faac07aac0a82feb
MistaKitty <tiago.abreu.laibacas@gmail.com>
2024-07-17 17:58:14 +0100

    Added product.model file

---
+++ models/product.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+const Item = mongoose.model('Item', itemSchema);
+module.exports = Item;
